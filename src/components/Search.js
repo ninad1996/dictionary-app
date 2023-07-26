68279c5adec08d3fb1b1887bff9545937d8e99ad
ninad1996 <ninad.sabnis16@gmail.com>
2023-07-26 18:52:59 +0530

    api call bug fix

--- src/components/Search.js
+++ src/components/Search.js
@@ -32,16 +32,10 @@ const Search = () => {
     useEffect(()=>{
         if(meaning != ""){
             // https://trends.google.com/trends/api/widgetdata/comparedgeo?hl=en-US&tz=-330&req=%7B%22geo%22:%7B%22country%22:%22IN%22%7D,%22comparisonItem%22:%5B%7B%22time%22:%222023-07-22T11%5C%5C:07%5C%5C:46+2023-07-23T11%5C%5C:07%5C%5C:46%22,%22complexKeywordsRestriction%22:%7B%22keyword%22:%5B%7B%22type%22:%22BROAD%22,%22value%22:%22book%22%7D%5D%7D%7D%5D,%22resolution%22:%22REGION%22,%22locale%22:%22en-US%22,%22requestOptions%22:%7B%22property%22:%22%22,%22backend%22:%22CM%22,%22category%22:0%7D,%22userConfig%22:%7B%22userType%22:%22USER_TYPE_LEGIT_USER%22%7D%7D&token=APP6_UEAAAAAZL5bgtXjbW0OecB57GqZYAHa5-ja0cdw
-            axios.get(`http://localhost:5000/api/browser?word=${wordData}`).then((response)=>{
+            axios.get(`https://dictionary-app-jet-six.vercel.app/api/browser?word=${wordData}`).then((response)=>{
                 console.log(response);
                 let json = response.data.comparedByRegion;
                 console.log(json)
-                // console.log(json);
-                // json = json.replace(/\)]}',\n/, "");
-                // console.log(json);
-                // json = JSON.parse(json);
-                // console.log(json);
-                // console.log(JSON.parse(json));
                 setMap(json);
             }).catch((err)=> {
                 console.log(err);
